a9f6ff3c600c4f697392788fc9724b70cba59e0b
stevenwjy <stevenwjy149@gmail.com>
2019-04-04 17:30:59 +0800

    Fix runner.py, remove reading input from length_extension_attack function.

--- hash/runner.py
+++ hash/runner.py
@@ -1,13 +1,7 @@
-def length_extension_attack(hash_algo):
+def length_extension_attack(hash_algo, msg, hash_val, ext, secret_length=0):
     if hash_algo == 'sha-256':
         from Sha256 import Sha256
         
-        secret_length   = int(input('Enter unknown initial message length (in bits): '))
-        msg             = input('Enter known initial message: ')
-        hash_val        = input('Enter initial hash value: ')
-
-        ext             = input('Enter additional message: ')
-
         msg_ln, pad_ln = Sha256.get_nbit(msg)
 
         msg_ln  = hex(msg_ln + secret_length)[2:].zfill(16)
@@ -37,5 +31,3 @@ def length_extension_attack(hash_algo):
 
         print("final hash value:")
         print(sha.hexdigest())
-
-length_extension_attack('sha-256')
